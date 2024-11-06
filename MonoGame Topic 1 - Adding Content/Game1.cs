f9056a41ac1f906ac4a448da6305316e2ba40ed5
Sam Ahrens <339729139@GoTVDSB.ca>
2024-11-06 09:39:32 -0500

    almost done. Need Ghost random

--- MonoGame Topic 1 - Adding Content/Game1.cs
+++ MonoGame Topic 1 - Adding Content/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace MonoGame_Topic_1___Adding_Content
 {
@@ -12,11 +14,19 @@ namespace MonoGame_Topic_1___Adding_Content
         Texture2D dinoTexture;
         Texture2D dinoDeadTexture;
         Texture2D skylineTexture;
+        Texture2D fireTexture;
+        Texture2D halloweenTexture;
+        Texture2D zombieTexture;
+        Texture2D moonTexture;
+        Texture2D ghostTexture;
+        private int whichPicture;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
         }
 
         protected override void Initialize()
@@ -26,7 +36,8 @@ namespace MonoGame_Topic_1___Adding_Content
             _graphics.PreferredBackBufferHeight = 485;
             _graphics.ApplyChanges();
 
-
+            Random generator = new Random();
+            whichPicture = generator.Next(2);
             this.Window.Title = "First MonoGame Project";
 
 
@@ -39,8 +50,12 @@ namespace MonoGame_Topic_1___Adding_Content
 
             dinoTexture = Content.Load<Texture2D>("dino");
             dinoDeadTexture = Content.Load<Texture2D>("dinoDead");
-            skylineTexture = Content.Load<Texture2D>("skyline")
-
+            skylineTexture = Content.Load<Texture2D>("skyline");
+            fireTexture = Content.Load<Texture2D>("fire");
+            halloweenTexture = Content.Load<Texture2D>("HalloweenBG");
+            zombieTexture = Content.Load<Texture2D>("zombie");
+            moonTexture = Content.Load<Texture2D>("moon");
+            ghostTexture = Content.Load<Texture2D>("ghost");
             // TODO: use this.Content to load your game content here
         }
 
@@ -58,10 +73,25 @@ namespace MonoGame_Topic_1___Adding_Content
         {
             GraphicsDevice.Clear(Color.BlanchedAlmond);
             _spriteBatch.Begin();
-
-
-            _spriteBatch.Draw(dinoTexture, new Vector2(500, 500), Color.White);
-
+            if (whichPicture == 0) //Godzilla mourns death of fellow Godzilla, destroys city
+            {
+                _spriteBatch.Draw(skylineTexture, new Vector2(0, 0), Color.White);
+                _spriteBatch.Draw(dinoTexture, new Vector2(370, 130), Color.White);
+                _spriteBatch.Draw(dinoDeadTexture, new Vector2(300, 325), Color.Red);
+                _spriteBatch.Draw(fireTexture, new Vector2(150, 330), Color.White);
+                _spriteBatch.Draw(fireTexture, new Vector2(700, 330), Color.White);
+                _spriteBatch.Draw(fireTexture, new Vector2(0, 330), Color.White);
+                _spriteBatch.Draw(fireTexture, new Vector2(175, 87), Color.White);
+                _spriteBatch.Draw(fireTexture, new Vector2(300, 133), Color.White);
+                _spriteBatch.Draw(fireTexture, new Vector2(730, 110), Color.White);
+            }
+            else if (whichPicture == 1) //Halloween
+            {
+                _spriteBatch.Draw(halloweenTexture, new Vector2(0, 0), Color.White);
+                _spriteBatch.Draw(zombieTexture, new Vector2(755, 265), Color.White);
+                _spriteBatch.Draw(moonTexture, new Vector2(40, -25), Color.White);
+                _spriteBatch.Draw(ghostTexture, new Vector2(400, 55), Color.White);
+            }
             _spriteBatch.End();
 
             // TODO: Add your drawing code here
